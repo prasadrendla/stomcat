f073fc15516c299c16b2f3bff0b64c5b8be8f4c1
prasadrendla <128022422+prasadrendla@users.noreply.github.com>
2023-06-26 18:38:10 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -8,10 +8,10 @@ sudo mv apache-tomcat-9.0.76 tomcat9
 sudo chown -R ubuntu:ubuntu tomcat9
 sudo chmod +x tomcat9/bin/*
 ./tomcat9/bin/startup.sh
- sudo sed -i '21,22 s/^/<!-- /; 21,22 s/$/ -->/' /home/ubuntu/tomcat9/webapps/manager/META-INF/context.xml
-sudo sed -i '21,22 s/^/<!-- /; 21,22 s/$/ -->/' /home/ubuntu/tomcat9/webapps/host-manager/META-INF/context.xml
+ sudo sed -i '21,22 s/^/<!-- /; 21,22 s/$/ -->/' /home/ubuntu/stomcat/tomcat9/webapps/manager/META-INF/context.xml
+sudo sed -i '21,22 s/^/<!-- /; 21,22 s/$/ -->/' /home/ubuntu/stomcat/tomcat9/webapps/host-manager/META-INF/context.xml
 sed -i '/<\/tomcat-users>/i \
 <role rolename="manager-gui"/>\
 <role rolename="admin-gui"/>\
 <user username="manager" password="12345" roles="manager-gui"/>\
-<user username="admin" password="12345" roles="admin-gui"/>' /home/ubuntu/tomcat9/conf/tomcat-users.xml
+<user username="admin" password="12345" roles="admin-gui"/>' /home/ubuntu/stomcat/tomcat9/conf/tomcat-users.xml
