c0b450f7ddd6180bf55a0803fb623f2a9bab147a
prasadrendla <128022422+prasadrendla@users.noreply.github.com>
2023-06-27 18:06:13 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -5,16 +5,17 @@ sudo apt install openjdk-17-jre-headless -y
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.76/bin/apache-tomcat-9.0.76.zip
 sudo unzip apache-tomcat-9.0.76.zip
 sudo mv apache-tomcat-9.0.76 tomcat9
-sudo chown -R ubuntu:ubuntu tomcat9
-sudo chmod +x tomcat9/bin/*
-./tomcat9/bin/startup.sh
- sudo sed -i '21,22 s/^/<!-- /; 21,22 s/$/ -->/' /home/ubuntu/tomcat9/webapps/manager/META-INF/context.xml
-sudo sed -i '21,22 s/^/<!-- /; 21,22 s/$/ -->/' /home/ubuntu/tomcat9/webapps/host-manager/META-INF/context.xml
+sudo mv tomcat9 /opt
+sudo chown -R ubuntu:ubuntu /opt/tomcat9
+sudo chmod +x /opt/tomcat9/bin/*
+./opt/tomcat9/bin/startup.sh
+ sudo sed -i '21,22 s/^/<!-- /; 21,22 s/$/ -->/' /opt/tomcat9/webapps/manager/META-INF/context.xml
+sudo sed -i '21,22 s/^/<!-- /; 21,22 s/$/ -->/' /opt/tomcat9/webapps/host-manager/META-INF/context.xml
 sed -i '/<\/tomcat-users>/i \
 <role rolename="manager-gui"/>\
 <role rolename="admin-gui"/>\
 <user username="manager" password="12345" roles="manager-gui"/>\
-<user username="admin" password="12345" roles="admin-gui"/>' /home/ubuntu/tomcat9/conf/tomcat-users.xml
-./tomcat9/bin/catalina.sh
-./tomcat9/bin/shutdown.sh
-./tomcat9/bin/startup.sh
+<user username="admin" password="12345" roles="admin-gui"/>' /opt/tomcat9/conf/tomcat-users.xml
+./opt/tomcat9/bin/catalina.sh
+./opt/tomcat9/bin/shutdown.sh
+./opt/tomcat9/bin/startup.sh
